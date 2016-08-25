40475df995e908def0b72aa35e10f0c4b21d6f2e
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2016-08-25 12:08:07 -0700

    output cif files and minor cleanup

--- cert_issuer/v2_issuer.py
+++ cert_issuer/v2_issuer.py
@@ -10,7 +10,6 @@ from cert_issuer.models import TransactionData
 from cert_issuer.models import convert_file_name
 from merkleproof.MerkleTree import MerkleTree
 from merkleproof.MerkleTree import sha256
-from merkleproof.utils import get_buffer
 
 
 if sys.version_info.major < 3:
@@ -23,8 +22,8 @@ sent_tx_file_name = 'sent_tx.txt'
 
 
 class V2Issuer(Issuer):
-    def __init__(self, config):
-        Issuer.__init__(self, config)
+    def __init__(self, config, certificates_to_issue):
+        Issuer.__init__(self, config, certificates_to_issue)
         self.batch_id = '%024x' % random.randrange(16 ** 24)
         self.tree = MerkleTree(hash_f=sha256)
 
@@ -32,41 +31,71 @@ class V2Issuer(Issuer):
         cert_utf8 = certificate.decode('utf-8')
         cert_json = json.loads(cert_utf8)
         normalized = jsonld.normalize(cert_json, {'algorithm': 'URDNA2015', 'format': 'application/nquads'})
-        hashed_bytes = get_buffer(sha256(normalized))
-        self.tree.add_leaf(hashed_bytes, True)
-        return hashed_bytes
+        hashed = sha256(normalized)
+        self.tree.add_leaf(hashed, True)
+        return hashed
 
     def get_cost_for_certificate_batch(self, dust_threshold, recommended_fee_per_transaction, satoshi_per_byte,
-                                       num_certificates, allow_transfer):
-        """
+                                       allow_transfer):
+        '''
         Per certificate, we pay 2*min_per_output (which is based on dust) + fee. Note assumes 1 input
         per tx. We may also need to pay additional fees for splitting into temp addresses
-        """
+        '''
+        num_certificates = len(self.certificates_to_issue)
         num_outputs = Issuer.get_num_outputs(num_certificates)
         return Issuer.get_cost_for_certificate_batch(dust_threshold, recommended_fee_per_transaction, satoshi_per_byte,
                                                      num_outputs, allow_transfer, 1, 1)
 
-    def create_transactions(self, wallet, revocation_address, certificates_to_issue, issuing_transaction_cost,
+    # TODO: move to merkleproof utils
+    def make_receipt(self, index, txid):
+
+        receipt = {
+            '@type': 'BlockchainReceipt',
+            'type': 'ChainpointSHA256v2',
+            'targetHash': hexlify(self.tree.get_leaf(index)),
+            'merkleRoot': self.tree.get_merkle_root(),
+            'proof': self.tree.get_proof(index),
+            'anchors': [
+                {
+                    'type': 'BTCOpReturn',
+                    'sourceId': txid
+                }
+            ]
+        }
+        return receipt
+
+
+    def finish_tx(self, sent_tx_file_name, txid):
+        Issuer.finish_tx(self, sent_tx_file_name, txid)
+        # note that certificates are stored in an ordered dictionary, so we will iterate in the same order
+        index = 0
+        for uid, certificate in self.certificates_to_issue.items():
+
+            receipt = self.make_receipt(index, txid)
+
+            receipt_file_name = convert_file_name(self.config.receipts_file_pattern, uid)
+            with open(receipt_file_name, 'w') as out_file:
+                out_file.write(json.dumps(receipt))
+
+            signed_cert_file_name = convert_file_name(self.config.signed_certs_file_pattern, uid)
+            with open(signed_cert_file_name, 'r') as in_file:
+                signed_cert = json.load(in_file)
+
+            signed_cert['blockchainreceipt'] = receipt
+            cif_file_name = convert_file_name(self.config.cifs_file_pattern, uid)
+
+            with open(cif_file_name, 'w') as out_file:
+                out_file.write(json.dumps(signed_cert))
+
+            index+=1
+
+
+
+    def create_transactions(self, wallet, revocation_address, issuing_transaction_cost,
                             split_input_trxs):
         # finish tree
         self.tree.make_tree()
-
         # convert_file_name(self.config.tree_file_pattern, self.batch_id)
-
-        # note that certificates are stored in an ordered dictionary, so we will iterate in the same order
-        index = 0
-        for uid, certificate in certificates_to_issue.items():
-
-            # TODO: fix
-            receipt = {'target': hexlify(self.tree.get_leaf(index)),
-                       'root': self.tree.get_merkle_root(),
-                       'proof': self.tree.get_proof(index)
-                       }
-
-            receipt_file_name = convert_file_name(self.config.proof_file_pattern, uid)
-            with open(receipt_file_name, 'w') as out_file:
-                out_file.write(json.dumps(receipt))
-            index+=1
 
         op_return_value = unhexlify(self.tree.get_merkle_root())
 
@@ -74,7 +103,6 @@ class V2Issuer(Issuer):
         last_output = unspent_outputs[-1]
 
         txouts = self.build_txouts(
-            certificates_to_issue,
             issuing_transaction_cost,
             revocation_address)
         tx = trx_utils.create_trx(
@@ -87,7 +115,7 @@ class V2Issuer(Issuer):
         unsigned_tx_file_name = convert_file_name(
             self.config.unsigned_txs_file_pattern, self.batch_id)
         unsent_tx_file_name = convert_file_name(
-            self.config.unsent_txs_file_pattern, self.batch_id)
+            self.config.signed_txs_file_pattern, self.batch_id)
         sent_tx_file_name = convert_file_name(
             self.config.sent_txs_file_pattern, self.batch_id)
 
@@ -101,10 +129,9 @@ class V2Issuer(Issuer):
 
         return [td]
 
-    def build_txouts(self, certificates_to_issue,
-                     issuing_transaction_cost, revocation_address):
+    def build_txouts(self, issuing_transaction_cost, revocation_address):
         txouts = []
-        for uid, certificate in certificates_to_issue.items():
+        for uid, certificate in self.certificates_to_issue.items():
             txouts = txouts + trx_utils.create_recipient_outputs(
                 certificate.pubkey, revocation_address, issuing_transaction_cost.min_per_output)
 
