345a47d0bd30f2045b25e29a2124858348f393a2
Kim Hamilton Duffy <kimdhamilton@gmail.com>
2017-03-08 01:52:31 -0800

    remove comment

--- tests/test_tx_utils.py
+++ tests/test_tx_utils.py
@@ -48,7 +48,6 @@ class TestTrxUtils(unittest.TestCase):
         b = h2b('8443b07464c762d7fb404ea918a5ac9b3618d5cd6a0c5ea6e4dd5d7bbe28b154')
         tx_input = Spendable(200, b'18eKkAWyU9kvRNHPKxnZb6wwtPMrNmRRRA',
                              b, 0)
-        #spendables = [Spendable(COIN_VALUE, standard_tx_out_script(BITCOIN_ADDRESSES[0]), FAKE_HASHES[1], 0)]
         tx_outs = [tx_utils.create_transaction_output('mgAqW5ZCnEp7fjvpj8RUL3WxsBy8rcDcCi', 0.0000275)]
         tx = tx_utils.create_trx('TEST'.encode('utf-8'), 3, 'mgAqW5ZCnEp7fjvpj8RUL3WxsBy8rcDcCi', tx_outs, tx_input)
         hextx = hexlify(tx.serialize())
