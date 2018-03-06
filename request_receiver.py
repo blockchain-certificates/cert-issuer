a681a47b003ea27087b38bd1d8e812e097991c88
Yancy Ribbens <yancy.ribbens@gmail.com>
2018-03-06 17:55:22 -0600

    add blockcert entry point

--- request_receiver.py
+++ request_receiver.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
+__requires__ = 'cert-issuer==2.0.11'
+import re
+import sys
+from pkg_resources import load_entry_point
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    load_entry_point('cert-issuer==2.0.11', 'console_scripts', 'cert-issuer')()
     return "Hello, World!"
 
 if __name__ == '__main__':
