9db883e83c5479f342babacfb57ee9fb03e5a799
Yancy Ribbens <yancy.ribbens@gmail.com>
2018-04-15 14:57:47 -0500

    remove old routes

--- cert_issuer.py
+++ cert_issuer.py
@@ -13,31 +13,11 @@ import cert_issuer.issue_certificates
 app = Flask(__name__)
 config = None
 
-info_json = {
-    'Author': u'Yancy Ribbens',
-    'description': u'REST API for cert-issuer',
-    'version': u'Pilot'
-    }
-
 def get_config():
     global config
     if config == None:
         config = cert_issuer.config.get_config()
     return config
-
-@app.route('/cert_issuer/api/v1.0/issue_cert', methods=['POST'])
-def create_cert():
-    if not request.json or not 'id' in request.json:
-        abort(400)
-    file_name = request.json['id'] + '.json' 
-    full_path_name = '/etc/cert-issuer/data/unsigned_certificates/' + file_name
-    with open(full_path_name, 'w') as outfile:
-        json.dump(request.json, outfile)
-    call('cert-issuer')
-    os.remove(full_path_name)
-    with open('/etc/cert-issuer/data/blockchain_certificates/' + file_name) as data_file:
-        blockchain_cert = json.load(data_file)
-    return jsonify(blockchain_cert), 201
 
 @app.route('/cert_issuer/api/v1.0/issue', methods=['POST'])
 def issue():
@@ -47,21 +27,6 @@ def issue():
     certificate_batch_handler.set_certificates_in_batch(request.json)
     cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler)
     return json.dumps(certificate_batch_handler.proof)
-
-@app.route('/cert_issuer/api/v1.0/certs/<string:cert_id>', methods=['GET'])
-def get_blockchain_cert(cert_id):
-    with open('/etc/cert-issuer/data/blockchain_certificates/' + cert_id + '.json') as data_file:
-        blockchain_cert = json.load(data_file)
-    return json.dumps(blockchain_cert)
-
-@app.route('/cert_issuer/api/v1.0/info', methods=['GET'])
-def info():
-    config = get_config()
-    config_dict = vars(config)
-    #TODO TypeError: Object of type 'Chain' is not JSON serializable
-    # del config_dict['chain']
-    #return jsonify({**info_json, 'config': config_dict })
-    return "todo"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
