ac9af919ee78f5003eb38003d1b584def29c6b94
Anthony Ronning <anthonyronning@gmail.com>
2018-05-09 13:19:37 -0500

    Removed ethereum testnet, should use ropsten instead

--- cert_issuer/issue_certificates.py
+++ cert_issuer/issue_certificates.py
@@ -28,7 +28,7 @@ def issue(app_config, certificate_batch_handler, transaction_handler):
 
 def main(app_config):
     chain = app_config.chain
-    if chain == Chain.ethereum_mainnet or chain == Chain.ethereum_ropsten or chain == Chain.ethereum_testnet:
+    if chain == Chain.ethereum_mainnet or chain == Chain.ethereum_ropsten:
         from cert_issuer import ethereum
         certificate_batch_handler, transaction_handler, connector = ethereum.instantiate_blockchain_handlers(app_config)
     else:
