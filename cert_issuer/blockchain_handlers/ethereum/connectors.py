830ee9d8ff2860976138dca8f2440c571e5d2170
AnthonyRonning <anthonyronning@gmail.com>
2020-02-19 15:44:27 -0600

    RI etherscan changes from v2 to v3

--- cert_issuer/blockchain_handlers/ethereum/connectors.py
+++ cert_issuer/blockchain_handlers/ethereum/connectors.py
@@ -95,12 +95,14 @@ class EtherscanBroadcaster(object):
 
         broadcast_url = self.base_url + '?module=proxy&action=eth_sendRawTransaction'
         if api_token:
-            '&apikey=%s' % api_token
+            broadcast_url += '&apikey=%s' % api_token
         response = requests.post(broadcast_url, data={'hex': tx_hex})
         if 'error' in response.json():
             logging.error("Etherscan returned an error: %s", response.json()['error'])
             raise BroadcastError(response.json()['error'])
         if int(response.status_code) == 200:
+            if response.json().get('message', None) == 'NOTOK':
+                raise BroadcastError(response.json().get('result', None))
             tx_id = response.json().get('result', None)
             logging.info("Transaction ID obtained from broadcast through Etherscan: %s", tx_id)
             return tx_id
@@ -116,9 +118,11 @@ class EtherscanBroadcaster(object):
         broadcast_url += '&address=%s' % address
         broadcast_url += '&tag=latest'
         if api_token:
-            '&apikey=%s' % api_token
+            broadcast_url += '&apikey=%s' % api_token
         response = requests.get(broadcast_url)
         if int(response.status_code) == 200:
+            if response.json().get('message', None) == 'NOTOK':
+                raise BroadcastError(response.json().get('result', None))
             balance = int(response.json().get('result', None))
             logging.info('Balance check succeeded: %s', response.json())
             return balance
@@ -133,10 +137,11 @@ class EtherscanBroadcaster(object):
         broadcast_url += '&address=%s' % address
         broadcast_url += '&tag=latest'
         if api_token:
-            '&apikey=%s' % api_token
+            broadcast_url += '&apikey=%s' % api_token
         response = requests.get(broadcast_url, )
         if int(response.status_code) == 200:
-            # the int(res, 0) transforms the hex nonce to int
+            if response.json().get('message', None) == 'NOTOK':
+                raise BroadcastError(response.json().get('result', None))
             nonce = int(response.json().get('result', None), 0)
             logging.info('Nonce check went correct: %s', response.json())
             return nonce
