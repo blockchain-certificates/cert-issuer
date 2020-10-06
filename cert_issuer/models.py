0d5a8424ae3cf983033b36644deaee4b224cfaab
Julien Fraichot <jfraichot@learningmachine.com>
2020-10-06 15:30:16 +0200

    feat(VC): validate credentialSubject property

--- cert_issuer/models.py
+++ cert_issuer/models.py
@@ -8,8 +8,11 @@ def validate_type (certificate_type):
         raise ValueError('`type` property should be an array')
     if isinstance(certificate_type, list) and len(certificate_type) <= 1:
         raise ValueError('`type` property should be an array with at least 2 values')
-    if isinstance(certificate_type, list) and certificate_metadata['type'][0] not in compulsory_types:
+    if isinstance(certificate_type, list) and certificate_type[0] not in compulsory_types:
         raise ValueError('`type` property first value should be either VerifiableCredential or VerifiablePresentation')
+    pass
+
+def validate_credential_subject (credential_subject):
     pass
 
 class BatchHandler(object):
@@ -35,6 +38,11 @@ class CertificateHandler(object):
     @abstractmethod
     def validate_certificate(self, certificate_metadata):
         validate_type(certificate_metadata['type'])
+        try:
+            # if undefined will throw KeyError
+            validate_credential_subject(certificate_metadata['credentialSubject'])
+        except:
+            raise ValueError('`credentialSubject property must be defined`')
         pass
 
     @abstractmethod
