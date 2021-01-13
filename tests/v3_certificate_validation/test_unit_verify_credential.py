872004840cbf8bed355437c58be6ecd55b6a9b92
Julien Fraichot <jfraichot@learningmachine.com>
2021-01-13 17:13:23 +0100

    test(Validation): add verify_presentation tests

--- tests/v3_certificate_validation/test_unit_verify_credential.py
+++ tests/v3_certificate_validation/test_unit_verify_credential.py
@@ -21,13 +21,6 @@ credential_example = {
      "alumniOf": {
        "id": "did:example:c276e12ec21ebfeb1f712ebc6f1"
      }
-   },
-   "proof": {
-     "type": "MerkleProof2019",
-     "created": "2020-03-11T09:48:20.304161",
-     "proofValue": "z2LuLBVSfnVzaQtvzuA7EaPQsGEgYWeaMTH1p3uqAG3ESx9HYyFzFFrYsyPkZSbn1Ji5LN76jw6HBr3oiaa8KsQenCPqKk7dJvxEXsDnYvhuDHtsDaQdmCyvpTR9oH46UZcCZ1UY7uZrgHmzf3J8Mzpp5Nnzd4SWiVN4RDWfxSkKmcoXywZ1pTm5bhbKAx1Xeydjwf7T7gcSSkUxQJmfJrWKdyiBjU1vt4oZxwbeTRQ9TfojiDRKJ6RPNsVPpkcDqGvPoaF58SQJG9xr8ACAAH9ZhYXJhRwW2zLpHGdRgyFGdxrcNiBVJ1o1TLcwLsfXTdRZLV2gW5yPLbEui6yBsmHtw9pQkWtfMxGBLzHk5ZRVLMdgUKatiV2QS4oE9N2GyiVnmQomApdS8R2cDSbQdn",
-     "proofPurpose": "assertionMethod",
-     "verificationMethod": "ecdsa-koblitz-pubkey:0x7e30a37763e6Ba1fFeDE1750bBeFB4c60b17a1B3"
    }
 }
 
