ad52583be48319597c3a0b95cce29a6b7abf788e
Julien Fraichot <jfraichot@learningmachine.com>
2021-01-25 18:06:41 +0100

    test(V3): test whole VC verification execution

--- tests/v3_certificate_validation/test_integration_batch_issuance_validation.py
+++ tests/v3_certificate_validation/test_integration_batch_issuance_validation.py
@@ -27,7 +27,45 @@ credential_example = {
 }
 
 class TestIssuanceBatchValidation (unittest.TestCase):
-    def test_missing_credential_subject (self):
+    def test_verify_type (self):
+        candidate = copy.deepcopy(credential_example)
+        candidate['type'] = 'Invalid Shape'
+        handler = CertificateBatchHandler(
+            secret_manager=mock.Mock(),
+            certificate_handler=MockCertificateV3Handler(candidate),
+            merkle_tree=mock.Mock(),
+            config=mock.Mock()
+        )
+        handler.certificates_to_issue = {'metadata': mock.Mock()}
+
+        try:
+            handler.prepare_batch()
+        except Exception as e:
+            self.assertEqual(str(e), '`type` property must be an array')
+            return
+
+        assert False
+
+    def test_verify_context (self):
+        candidate = copy.deepcopy(credential_example)
+        candidate['@context'] = 'Invalid Shape'
+        handler = CertificateBatchHandler(
+            secret_manager=mock.Mock(),
+            certificate_handler=MockCertificateV3Handler(candidate),
+            merkle_tree=mock.Mock(),
+            config=mock.Mock()
+        )
+        handler.certificates_to_issue = {'metadata': mock.Mock()}
+
+        try:
+            handler.prepare_batch()
+        except Exception as e:
+            self.assertEqual(str(e), '`@context` property must be an array')
+            return
+
+        assert False
+
+    def test_verify_credential_subject (self):
         candidate = copy.deepcopy(credential_example)
         del candidate['credentialSubject']
         handler = CertificateBatchHandler(
@@ -41,7 +79,85 @@ class TestIssuanceBatchValidation (unittest.TestCase):
         try:
             handler.prepare_batch()
         except Exception as e:
-            self.assertEqual(str(e), '`credentialSubject property must be defined`')
+            self.assertEqual(str(e), '`credentialSubject` property must be defined')
+            return
+
+        assert False
+
+    def test_verify_issuer (self):
+        candidate = copy.deepcopy(credential_example)
+        del candidate['issuer']
+        handler = CertificateBatchHandler(
+            secret_manager=mock.Mock(),
+            certificate_handler=MockCertificateV3Handler(candidate),
+            merkle_tree=mock.Mock(),
+            config=mock.Mock()
+        )
+        handler.certificates_to_issue = {'metadata': mock.Mock()}
+
+        try:
+            handler.prepare_batch()
+        except Exception as e:
+            self.assertEqual(str(e), '`issuer` property must be defined')
+            return
+
+        assert False
+
+    def test_verify_issuance_date (self):
+        candidate = copy.deepcopy(credential_example)
+        del candidate['issuanceDate']
+        handler = CertificateBatchHandler(
+            secret_manager=mock.Mock(),
+            certificate_handler=MockCertificateV3Handler(candidate),
+            merkle_tree=mock.Mock(),
+            config=mock.Mock()
+        )
+        handler.certificates_to_issue = {'metadata': mock.Mock()}
+
+        try:
+            handler.prepare_batch()
+        except Exception as e:
+            self.assertEqual(str(e), '`issuanceDate` property must be defined')
+            return
+
+        assert False
+
+    def test_verify_expiration_date (self):
+        candidate = copy.deepcopy(credential_example)
+        candidate['expirationDate'] = '20200909'
+        handler = CertificateBatchHandler(
+            secret_manager=mock.Mock(),
+            certificate_handler=MockCertificateV3Handler(candidate),
+            merkle_tree=mock.Mock(),
+            config=mock.Mock()
+        )
+        handler.certificates_to_issue = {'metadata': mock.Mock()}
+
+        try:
+            handler.prepare_batch()
+        except Exception as e:
+            self.assertEqual(str(e), '`expirationDate` property must be a valid RFC3339 string')
+            return
+
+        assert False
+
+    def test_verify_credential_status (self):
+        candidate = copy.deepcopy(credential_example)
+        candidate['credentialStatus'] = {
+            "id": 'https://valid.path'
+        }
+        handler = CertificateBatchHandler(
+            secret_manager=mock.Mock(),
+            certificate_handler=MockCertificateV3Handler(candidate),
+            merkle_tree=mock.Mock(),
+            config=mock.Mock()
+        )
+        handler.certificates_to_issue = {'metadata': mock.Mock()}
+
+        try:
+            handler.prepare_batch()
+        except Exception as e:
+            self.assertEqual(str(e), 'credentialStatus.type must be defined')
             return
 
         assert False
