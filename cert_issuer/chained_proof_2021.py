1c6e3b4ad8cecc429afddd03a0fb1a9b09687d62
Julien Fraichot <julien.fraichot@hyland.com>
2022-05-09 15:20:50 -0500

    feat(MultiSign): allow n amount of proofs to be chained

--- cert_issuer/chained_proof_2021.py
+++ cert_issuer/chained_proof_2021.py
@@ -1,4 +1,4 @@
-import json
+CHAINED_PROOF_TYPE = 'ChainedProof2021'
 
 class ChainedProof2021:
     type = ''
@@ -9,14 +9,27 @@ class ChainedProof2021:
     previousProof = ''
     proofValue = ''
 
-    def __init__ (self, previousProof, currentProof):
-        self.type = 'ChainedProof2021'
-        self.verificationMethod = currentProof['verificationMethod']
-        self.chainedProofType = currentProof['type']
-        self.created = currentProof['created']
-        self.proofPurpose = currentProof['proofPurpose']
-        self.previousProof = previousProof
-        self.proofValue = currentProof['proofValue']
+    def __init__(self, previous_proof, current_proof):
+        self.type = CHAINED_PROOF_TYPE
+        self.create_proof_object(current_proof)
+        self.set_previous_proof(previous_proof)
 
-    def toJsonObject(self):
+    def create_proof_object(self, current_proof):
+        self.verificationMethod = current_proof['verificationMethod']
+        self.chainedProofType = current_proof['type']
+        self.created = current_proof['created']
+        self.proofPurpose = current_proof['proofPurpose']
+        self.proofValue = current_proof['proofValue']
+
+    def set_previous_proof(self, previous_proof):
+        if previous_proof['type'] == CHAINED_PROOF_TYPE:
+            previous_proof_to_store = previous_proof
+            previous_proof_to_store['type'] = previous_proof['chainedProofType']
+            del previous_proof_to_store['chainedProofType']
+            del previous_proof_to_store['previousProof']
+            self.previousProof = previous_proof_to_store
+        else:
+            self.previousProof = previous_proof
+
+    def to_json_object(self):
         return self.__dict__
--- cert_issuer/proof_handler.py
+++ cert_issuer/proof_handler.py
@@ -3,8 +3,12 @@ from cert_issuer.chained_proof_2021 import ChainedProof2021
 class ProofHandler:
     def add_proof (self, certificate_json, merkle_proof):
         if 'proof' in certificate_json:
-            initial_proof = certificate_json['proof']
-            certificate_json['proof'] = [initial_proof, ChainedProof2021(initial_proof, merkle_proof).toJsonObject()]
+            if not isinstance(certificate_json['proof'], list):
+                # convert proof to list
+                initial_proof = certificate_json['proof']
+                certificate_json['proof'] = [initial_proof]
+            previous_proof = certificate_json['proof'][-1]
+            certificate_json['proof'].append(ChainedProof2021(previous_proof, merkle_proof).to_json_object())
         else:
             certificate_json['proof'] = merkle_proof
         return certificate_json
