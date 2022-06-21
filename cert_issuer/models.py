41f1797f6152052d785da16e20837c05abc1cfb1
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-21 15:01:20 -0500

    fix(RFC3339): fix regex to differentiate closing group Z or timezone offset

--- cert_issuer/models.py
+++ cert_issuer/models.py
@@ -7,7 +7,7 @@ from cert_issuer.config import ESTIMATE_NUM_INPUTS
 
 # TODO: move the v3 checks to cert-schema
 def validate_RFC3339_date (date):
-    return re.match('^[1-9]\d{3}-\d{2}-\d{2}[Tt\s]\d{2}:\d{2}:\d{2}(?:\.\d{3})?(?:[+-]\d{2}:\d{2})?[Zz]$', date)
+    return re.match('^[1-9]\d{3}-\d{2}-\d{2}[Tt\s]\d{2}:\d{2}:\d{2}(?:\.\d{3})?((?:[+-]\d{2}:\d{2})|[Zz])$', date)
 
 def is_valid_url (url):
     try:
