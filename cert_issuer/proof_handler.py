9f75673584645693192377cc35f77bbe06464bb4
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-21 10:42:13 -0500

    feat(MultiSign): update context to reflect signature suites

--- cert_issuer/proof_handler.py
+++ cert_issuer/proof_handler.py
@@ -1,4 +1,6 @@
 from cert_issuer.chained_proof_2021 import ChainedProof2021
+from cert_schema import ContextUrls
+from cert_issuer.utils import array_intersect
 
 class ProofHandler:
     def add_proof (self, certificate_json, merkle_proof):
@@ -9,6 +11,23 @@ class ProofHandler:
                 certificate_json['proof'] = [initial_proof]
             previous_proof = certificate_json['proof'][-1]
             certificate_json['proof'].append(ChainedProof2021(previous_proof, merkle_proof).to_json_object())
+            self.update_context_for_chained_proof(certificate_json)
         else:
             certificate_json['proof'] = merkle_proof
         return certificate_json
+
+    def update_context_for_chained_proof (self, certificate_json):
+        context = certificate_json['@context']
+        contextUrlsInstance = ContextUrls()
+        if contextUrlsInstance.merkle_proof_2019() not in context:
+            context.append(contextUrlsInstance.merkle_proof_2019())
+
+        if contextUrlsInstance.chained_proof_2021() not in context:
+            context.append(contextUrlsInstance.chained_proof_2021())
+
+        if array_intersect(contextUrlsInstance.v3_all(), context):
+            for v3_context in contextUrlsInstance.v3_all():
+                if v3_context in context:
+                    index = context.index(v3_context)
+                    del context[index]
+            context.append(contextUrlsInstance.v3_1_canonical())
--- tests/test_proof_handler.py
+++ tests/test_proof_handler.py
@@ -1,9 +1,31 @@
 import unittest
 from cert_issuer.proof_handler import ProofHandler
+from cert_schema import ContextUrls
 
 class TestProofHandler(unittest.TestCase):
+    def setUp(self):
+        self.handler = ProofHandler()
+        self.contextUrls = ContextUrls()
+
+    def test_single_signature(self):
+        fixture_certificate_json = {
+            '@context': [
+                'https://www.w3.org/2018/credentials/v1',
+                'https://w3id.org/blockcerts/v3'
+            ],
+            'kek': 'kek'
+        }
+        fixture_proof = {
+            'type': 'MerkleProof2019',
+            'created': '2022-05-05T08:05:14.912828',
+            'proofValue': 'zMcm4LfQFUZkWZyLJp1bqtXF8vkZZwp79x7Nvt5BmN2XV4usLLtDoeqiq3et923mcWfXde4a3m4f57yUZcATCbBXV1byb5AXbV8EzT6E8B9JKf3scvxxZCBVePtV4SrhYysAiLNJ9N2R8LgnpJ47wnQHkaTB1AMxrcLEHUTxm4zJTtQqf9orDLf3L4VoLzmST7ZzsDjuX9cw2hZ3Aazhhjy7swG44xfF1PC73SyCv77pDnJ6BSHm3azmbVG6BXv1EPtwF4J1YRqwojBEWk9nDgduACR7b9qNhQ46ND4B5vL8p3LkqTh',
+            'proofPurpose': 'assertionMethod',
+            'verificationMethod': 'did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ#key-1'
+        }
+        output = self.handler.add_proof(fixture_certificate_json, fixture_proof)
+        self.assertEqual(output['proof'], fixture_proof)
+
     def test_multiple_two_chained_signature(self):
-        handler = ProofHandler()
         fixture_initial_proof = {
           'type': 'Ed25519Signature2020',
           'created': '2022-05-02T16:36:22.933Z',
@@ -12,6 +34,10 @@ class TestProofHandler(unittest.TestCase):
           'proofValue': 'zAvFt59599JweBZ4zPP6Ge8LhKgECtBvDRmjG5VQbgEkPCiyMcM9QAPanJgSCs6RRGcKu96qNpfmpe9eTygpFZP6'
         }
         fixture_certificate_json = {
+            '@context': [
+                'https://www.w3.org/2018/credentials/v1',
+                'https://w3id.org/blockcerts/v3'
+            ],
             'kek': 'kek',
             'proof': fixture_initial_proof
         }
@@ -22,25 +48,21 @@ class TestProofHandler(unittest.TestCase):
             'proofPurpose': 'assertionMethod',
             'verificationMethod': 'did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ#key-1'
         }
-        output = handler.add_proof(fixture_certificate_json, fixture_proof)
-        self.assertEqual(output, {
-            'kek': 'kek',
-            'proof': [
-                fixture_initial_proof,
-                {
-                    'type': 'ChainedProof2021',
-                    'chainedProofType':  'MerkleProof2019',
-                    'created': '2022-05-05T08:05:14.912828',
-                    'previousProof': fixture_initial_proof,
-                    'proofValue': 'zMcm4LfQFUZkWZyLJp1bqtXF8vkZZwp79x7Nvt5BmN2XV4usLLtDoeqiq3et923mcWfXde4a3m4f57yUZcATCbBXV1byb5AXbV8EzT6E8B9JKf3scvxxZCBVePtV4SrhYysAiLNJ9N2R8LgnpJ47wnQHkaTB1AMxrcLEHUTxm4zJTtQqf9orDLf3L4VoLzmST7ZzsDjuX9cw2hZ3Aazhhjy7swG44xfF1PC73SyCv77pDnJ6BSHm3azmbVG6BXv1EPtwF4J1YRqwojBEWk9nDgduACR7b9qNhQ46ND4B5vL8p3LkqTh',
-                    'proofPurpose': 'assertionMethod',
-                    'verificationMethod': 'did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ#key-1'
-                }
-            ]
-        })
+        output = self.handler.add_proof(fixture_certificate_json, fixture_proof)
+        self.assertEqual(output['proof'], [
+            fixture_initial_proof,
+            {
+                'type': 'ChainedProof2021',
+                'chainedProofType':  'MerkleProof2019',
+                'created': '2022-05-05T08:05:14.912828',
+                'previousProof': fixture_initial_proof,
+                'proofValue': 'zMcm4LfQFUZkWZyLJp1bqtXF8vkZZwp79x7Nvt5BmN2XV4usLLtDoeqiq3et923mcWfXde4a3m4f57yUZcATCbBXV1byb5AXbV8EzT6E8B9JKf3scvxxZCBVePtV4SrhYysAiLNJ9N2R8LgnpJ47wnQHkaTB1AMxrcLEHUTxm4zJTtQqf9orDLf3L4VoLzmST7ZzsDjuX9cw2hZ3Aazhhjy7swG44xfF1PC73SyCv77pDnJ6BSHm3azmbVG6BXv1EPtwF4J1YRqwojBEWk9nDgduACR7b9qNhQ46ND4B5vL8p3LkqTh',
+                'proofPurpose': 'assertionMethod',
+                'verificationMethod': 'did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ#key-1'
+            }
+        ])
 
     def test_multiple_three_chained_signature(self):
-        handler = ProofHandler()
         fixture_initial_proof = {
           'type': 'Ed25519Signature2020',
           'created': '2022-05-02T16:36:22.933Z',
@@ -58,6 +80,10 @@ class TestProofHandler(unittest.TestCase):
             'verificationMethod': 'did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ#key-1'
         }
         fixture_certificate_json = {
+            '@context': [
+                'https://www.w3.org/2018/credentials/v1',
+                'https://w3id.org/blockcerts/v3'
+            ],
             'kek': 'kek',
             'proof': [
                 fixture_initial_proof,
@@ -71,27 +97,112 @@ class TestProofHandler(unittest.TestCase):
             'proofPurpose': 'assertionMethod',
             'verificationMethod': 'did:example:ebfeb1f712ebc6f1c276e12ec21#assertion'
         }
-        output = handler.add_proof(fixture_certificate_json, fixture_proof)
+        output = self.handler.add_proof(fixture_certificate_json, fixture_proof)
         self.maxDiff = None
-        self.assertEqual(output, {
+        self.assertEqual(output['proof'], [
+            fixture_initial_proof,
+            fixture_second_proof,
+            {
+                'type': 'ChainedProof2021',
+                'chainedProofType': 'MerkleProof2019',
+                'created': '2022-05-06T20:31:54',
+                'previousProof': {
+                    'type': 'MerkleProof2019',
+                    'created': '2022-05-05T08:05:14.912828',
+                    'proofValue': 'zMcm4LfQFUZkWZyLJp1bqtXF8vkZZwp79x7Nvt5BmN2XV4usLLtDoeqiq3et923mcWfXde4a3m4f57yUZcATCbBXV1byb5AXbV8EzT6E8B9JKf3scvxxZCBVePtV4SrhYysAiLNJ9N2R8LgnpJ47wnQHkaTB1AMxrcLEHUTxm4zJTtQqf9orDLf3L4VoLzmST7ZzsDjuX9cw2hZ3Aazhhjy7swG44xfF1PC73SyCv77pDnJ6BSHm3azmbVG6BXv1EPtwF4J1YRqwojBEWk9nDgduACR7b9qNhQ46ND4B5vL8p3LkqTh',
+                    'proofPurpose': 'assertionMethod',
+                    'verificationMethod': 'did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ#key-1'
+                },
+                'proofValue': 'mockProofValueForUnitTestPurpose',
+                'proofPurpose': 'assertionMethod',
+                'verificationMethod': 'did:example:ebfeb1f712ebc6f1c276e12ec21#assertion'
+            }
+        ])
+
+    def test_adds_merkle_proof_context(self):
+        fixture_initial_proof = {
+            'type': 'Ed25519Signature2020',
+            'created': '2022-05-02T16:36:22.933Z',
+            'verificationMethod': 'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs',
+            'proofPurpose': 'assertionMethod',
+            'proofValue': 'zAvFt59599JweBZ4zPP6Ge8LhKgECtBvDRmjG5VQbgEkPCiyMcM9QAPanJgSCs6RRGcKu96qNpfmpe9eTygpFZP6'
+        }
+        fixture_certificate_json = {
+            '@context': [
+                'https://www.w3.org/2018/credentials/v1',
+                'https://w3id.org/security/suites/ed25519-2020/v1',
+                'https://w3id.org/blockcerts/v3'
+            ],
             'kek': 'kek',
             'proof': [
-                fixture_initial_proof,
-                fixture_second_proof,
-                {
-                    'type': 'ChainedProof2021',
-                    'chainedProofType': 'MerkleProof2019',
-                    'created': '2022-05-06T20:31:54',
-                    'previousProof': {
-                        'type': 'MerkleProof2019',
-                        'created': '2022-05-05T08:05:14.912828',
-                        'proofValue': 'zMcm4LfQFUZkWZyLJp1bqtXF8vkZZwp79x7Nvt5BmN2XV4usLLtDoeqiq3et923mcWfXde4a3m4f57yUZcATCbBXV1byb5AXbV8EzT6E8B9JKf3scvxxZCBVePtV4SrhYysAiLNJ9N2R8LgnpJ47wnQHkaTB1AMxrcLEHUTxm4zJTtQqf9orDLf3L4VoLzmST7ZzsDjuX9cw2hZ3Aazhhjy7swG44xfF1PC73SyCv77pDnJ6BSHm3azmbVG6BXv1EPtwF4J1YRqwojBEWk9nDgduACR7b9qNhQ46ND4B5vL8p3LkqTh',
-                        'proofPurpose': 'assertionMethod',
-                        'verificationMethod': 'did:ion:EiA_Z6LQILbB2zj_eVrqfQ2xDm4HNqeJUw5Kj2Z7bFOOeQ#key-1'
-                    },
-                    'proofValue': 'mockProofValueForUnitTestPurpose',
-                    'proofPurpose': 'assertionMethod',
-                    'verificationMethod': 'did:example:ebfeb1f712ebc6f1c276e12ec21#assertion'
-                }
+                fixture_initial_proof
             ]
-        })
+        }
+        fixture_proof = {
+            'type': 'MerkleProof2019',
+            'created': '2022-05-06T20:31:54',
+            'proofValue': 'mockProofValueForUnitTestPurpose',
+            'proofPurpose': 'assertionMethod',
+            'verificationMethod': 'did:example:ebfeb1f712ebc6f1c276e12ec21#assertion'
+        }
+        output = self.handler.add_proof(fixture_certificate_json, fixture_proof)
+        self.assertIn(self.contextUrls.merkle_proof_2019(), output['@context'])
+
+    def test_adds_chained_proof_context(self):
+        fixture_initial_proof = {
+            'type': 'Ed25519Signature2020',
+            'created': '2022-05-02T16:36:22.933Z',
+            'verificationMethod': 'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs',
+            'proofPurpose': 'assertionMethod',
+            'proofValue': 'zAvFt59599JweBZ4zPP6Ge8LhKgECtBvDRmjG5VQbgEkPCiyMcM9QAPanJgSCs6RRGcKu96qNpfmpe9eTygpFZP6'
+        }
+        fixture_certificate_json = {
+            '@context': [
+                'https://www.w3.org/2018/credentials/v1',
+                'https://w3id.org/security/suites/ed25519-2020/v1',
+                'https://w3id.org/blockcerts/v3'
+            ],
+            'kek': 'kek',
+            'proof': [
+                fixture_initial_proof
+            ]
+        }
+        fixture_proof = {
+            'type': 'MerkleProof2019',
+            'created': '2022-05-06T20:31:54',
+            'proofValue': 'mockProofValueForUnitTestPurpose',
+            'proofPurpose': 'assertionMethod',
+            'verificationMethod': 'did:example:ebfeb1f712ebc6f1c276e12ec21#assertion'
+        }
+        output = self.handler.add_proof(fixture_certificate_json, fixture_proof)
+        self.assertIn(self.contextUrls.chained_proof_2021(), output['@context'])
+
+    def test_updates_blockcerts_context_version(self):
+        fixture_initial_proof = {
+            'type': 'Ed25519Signature2020',
+            'created': '2022-05-02T16:36:22.933Z',
+            'verificationMethod': 'did:key:z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs#z6MkjHnntGvtLjwfAMHWTAXXGJHhVL3DPtaT9BHmyTjWpjqs',
+            'proofPurpose': 'assertionMethod',
+            'proofValue': 'zAvFt59599JweBZ4zPP6Ge8LhKgECtBvDRmjG5VQbgEkPCiyMcM9QAPanJgSCs6RRGcKu96qNpfmpe9eTygpFZP6'
+        }
+        fixture_certificate_json = {
+            '@context': [
+                'https://www.w3.org/2018/credentials/v1',
+                'https://w3id.org/security/suites/ed25519-2020/v1',
+                'https://w3id.org/blockcerts/v3'
+            ],
+            'kek': 'kek',
+            'proof': [
+                fixture_initial_proof
+            ]
+        }
+        fixture_proof = {
+            'type': 'MerkleProof2019',
+            'created': '2022-05-06T20:31:54',
+            'proofValue': 'mockProofValueForUnitTestPurpose',
+            'proofPurpose': 'assertionMethod',
+            'verificationMethod': 'did:example:ebfeb1f712ebc6f1c276e12ec21#assertion'
+        }
+        output = self.handler.add_proof(fixture_certificate_json, fixture_proof)
+        self.assertNotIn(self.contextUrls.v3_canonical(), output['@context'])
+        self.assertIn(self.contextUrls.v3_1_canonical(), output['@context'])
