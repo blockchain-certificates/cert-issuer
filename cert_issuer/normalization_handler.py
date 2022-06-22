d7e1a6b2a215479d4d25485dcd9f490aec675e97
Julien Fraichot <julien.fraichot@hyland.com>
2022-06-22 12:19:08 -0500

    feat(MultiSign): register context before issuance

--- cert_issuer/normalization_handler.py
+++ cert_issuer/normalization_handler.py
@@ -1,8 +1,15 @@
-from cert_schema import normalize_jsonld
+import json
+import os
+from cert_schema import normalize_jsonld, extend_preloaded_context
+
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
 class JSONLDHandler:
     @staticmethod
     def normalize_to_utf8(certificate_json):
+        with open(os.path.join(BASE_DIR, '../data/context/ed25519.v1.json')) as context_file:
+            context_data = json.load(context_file)
+            extend_preloaded_context('https://w3id.org/security/suites/ed25519-2020/v1', context_data)
         normalized = normalize_jsonld(certificate_json, detect_unmapped_fields=False)
         return normalized.encode('utf-8')
