03b39aaedc42bec0afc1aaed9a44873f10afba18
Julien Fraichot <julien.fraichot@hyland.com>
2022-07-11 12:31:16 -0500

    feat(Metadata): check properties existence as defined from display order

--- cert_issuer/models/metadata.py
+++ cert_issuer/models/metadata.py
@@ -1,5 +1,8 @@
+import logging
+
 from jsonschema import validate
 from copy import copy
+
 
 def validate_metadata_structure(metadata):
     if 'schema' in metadata:
@@ -10,3 +13,38 @@ def validate_metadata_structure(metadata):
         except Exception as e:
             print(e)
             raise Exception('Certificate.metadata object does not match its provided schema')
+    else:
+       logging.warning("""
+            The metadata object provided with the certificate does not include a `schema` property.
+            Not defining such property will result in errors in the rendering of the metadata property in the UI projects. 
+            """)
+
+    if not 'displayOrder' in metadata:
+        logging.warning("""
+           The metadata object provided with the certificate does not include a `displayOrder` property.
+           Not defining such property will result in errors in the rendering of the metadata property in the UI projects. 
+           """)
+    else:
+        verify_display_order_properties(metadata['displayOrder'], metadata)
+
+
+def verify_display_order_properties(display_order, metadata):
+    for item in display_order:
+        path = item.split('.')
+        group = path[0]
+        if not group in metadata:
+            # \033[1m%s\033[0m: display property name in bold
+            logging.warning(
+                "`metadata.displayOrder` property references a group named: \033[1m%s\033[0m which does not exist in metadata object.",
+                group
+            )
+        else:
+            property = path[1]
+            if not property in metadata[group]:
+                logging.warning(
+                    "`metadata.displayOrder` property references a property named: \033[1m%s\033[0m which does not exist in group: \033[1m%s\033[0m.",
+                    property,
+                    group
+                )
+
+    pass
--- tests/models/test_metadata_validation.py
+++ tests/models/test_metadata_validation.py
@@ -1,6 +1,8 @@
 import json
+import logging
 import unittest
 from cert_issuer.models.metadata import validate_metadata_structure
+from kgb import SpyAgency
 
 class MetadataValidationTestSuite(unittest.TestCase):
     def test_json_schema_validation_is_valid(self):
@@ -22,3 +24,29 @@ class MetadataValidationTestSuite(unittest.TestCase):
             return
 
         assert False
+
+    def test_json_schema_validation_display_order(self):
+        spy = SpyAgency()
+        spy.spy_on(logging.warning)
+        metadata_string = "{\"schema\":{\"$schema\":\"http://json-schema.org/draft-04/schema#\",\"type\":\"object\",\"properties\":{\"displayOrder\":{\"type\":\"array\",\"items\":{\"type\":\"string\"}},\"certificate\":{\"order\":[],\"type\":\"object\",\"properties\":{\"issuingInstitution\":{\"title\":\"Issuing Institution\",\"type\":\"string\",\"default\":\"Learning Machine Technologies, Inc.\"}}},\"recipient\":{}}},\"certificate\":{\"issuingInstitution\":\"Learning Machine Technologies, Inc.\"},\"recipient\":{},\"displayOrder\":[\"certificate.issuingInstitution\", \"recipient.name\", \"class.year\"]}"
+        try:
+            validate_metadata_structure(json.loads(metadata_string))
+        except Exception as e:
+            assert False
+            logging.warning.unspy()
+            return
+
+        self.assertTrue(
+            logging.warning.calls[0].called_with(
+                '`metadata.displayOrder` property references a property named: \x1b[1m%s\x1b[0m which does not exist in group: \x1b[1m%s\x1b[0m.',
+                'name',
+                'recipient'
+            )
+        )
+        self.assertTrue(
+            logging.warning.calls[1].called_with(
+                '`metadata.displayOrder` property references a group named: \x1b[1m%s\x1b[0m which does not exist in metadata object.',
+                'class'
+            )
+        )
+        logging.warning.unspy()
