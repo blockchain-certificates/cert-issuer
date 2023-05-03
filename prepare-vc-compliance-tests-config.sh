2b82aae9a7b6851b37ce43dad02e7fbdb524ed9d
Julien Fraichot <julien.fraichot@hyland.com>
2023-05-03 14:57:27 +0200

    chore(CI): configure work dir

--- prepare-vc-compliance-tests-config.sh
+++ prepare-vc-compliance-tests-config.sh
@@ -2,6 +2,10 @@ echo "chain=mockchain" >> conf.ini
 echo "usb_name=${PWD}" >> conf.ini
 touch pk.txt
 echo "key_file=pk.txt" >> conf.ini
+mkdir data/unsigned_certificates
+mkdir data/blockchain_certificates
+echo "unsigned_certificates_dir=${PWD}/data/unsigned_certificates" >> conf.ini
+echo "blockchain_certificates_dir=${PWD}/data/blockchain_certificates" >> conf.ini
 
 echo "Prepared config file for VC compliance test suite"
 cat conf.ini
