2cfc03c77ff2a0a0ab0f12a61037326f935be684
Julien Fraichot <julien.fraichot@hyland.com>
2023-05-03 14:39:03 +0200

    chore(CI): add feedback after operation

--- prepare-vc-compliance-tests-config.sh
+++ prepare-vc-compliance-tests-config.sh
@@ -2,3 +2,6 @@ echo "chain=mockchain" >> conf.ini
 echo "usb_name=${PWD}" >> conf.ini
 touch pk.txt
 echo "key_file=pk.txt" >> conf.ini
+
+echo "Prepared config file for VC compliance test suite"
+cat conf.ini
