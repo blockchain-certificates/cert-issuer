a066fc54f2cf133d4560c3ff3f501621742c633e
Julien Fraichot <julien.fraichot@hyland.com>
2023-05-12 17:57:02 +0200

    test(Compliance): add keys for mocknet issuance

--- prepare-vc-compliance-tests-config.sh
+++ prepare-vc-compliance-tests-config.sh
@@ -1,11 +1,12 @@
 echo "chain=mockchain" >> conf.ini
 echo "usb_name=${PWD}" >> conf.ini
-touch pk.txt
+echo "5JwtrfUV5ZZe9z3q62whK4mQduxYpa7f25UXAKJCLTN1qTiRBSF" >> pk.txt #1KeZVz9iqpD9YeiExY2Wxu6XXexZ3pdxwe
 echo "key_file=pk.txt" >> conf.ini
 mkdir data/unsigned_certificates
 mkdir data/blockchain_certificates
 echo "unsigned_certificates_dir=${PWD}/data/unsigned_certificates" >> conf.ini
 echo "blockchain_certificates_dir=${PWD}/data/blockchain_certificates" >> conf.ini
+echo "no_safe_mode" >> conf.ini
 
 echo "Prepared config file for VC compliance test suite"
 cat conf.ini
