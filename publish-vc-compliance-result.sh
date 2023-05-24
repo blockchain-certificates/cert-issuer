c2f5e16d29c60d84217accc9b69bb2a2f3a7df7f
Julien Fraichot <julien.fraichot@hyland.com>
2023-05-24 17:40:56 +0200

    chore(Compliance): dwbug CI

--- publish-vc-compliance-result.sh
+++ publish-vc-compliance-result.sh
@@ -1,5 +1,7 @@
 cp node_modules/vc-test-suite/implementations/index.html ./vc-compliance-report.html
 git status
-git add ./vc-compliance-report.html
+git add vc-compliance-report.html
 git commit -m "chore(Compliance): update compliance report"
+git config --list
+echo "Pushing changes to $TRAVIS_BRANCH"
 git push origin $TRAVIS_BRANCH
