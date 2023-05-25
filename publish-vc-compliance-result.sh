e8115d47f13d8f054e308e377641ff41956b7554
Julien Fraichot <julien.fraichot@hyland.com>
2023-05-25 14:43:46 +0200

    chore(Compliance): debug commit content

--- publish-vc-compliance-result.sh
+++ publish-vc-compliance-result.sh
@@ -2,7 +2,7 @@ cp node_modules/vc-test-suite/implementations/index.html ./vc-compliance-report.
 git status
 git add vc-compliance-report.html
 git commit -m "chore(Compliance): update compliance report"
-git config --list
+git show -1
 echo "Pushing changes to $TRAVIS_BRANCH"
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
