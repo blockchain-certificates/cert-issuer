e1f8959e48a9401b49f075f1445a75a59b5046ca
Julien Fraichot <julien.fraichot@hyland.com>
2023-05-25 14:16:55 +0200

    chore(Compliance): attempt Github auth

--- publish-vc-compliance-result.sh
+++ publish-vc-compliance-result.sh
@@ -4,4 +4,6 @@ git add vc-compliance-report.html
 git commit -m "chore(Compliance): update compliance report"
 git config --list
 echo "Pushing changes to $TRAVIS_BRANCH"
+git config credential.helper "store --file=.git/credentials"
+echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 git push origin $TRAVIS_BRANCH --verbose
