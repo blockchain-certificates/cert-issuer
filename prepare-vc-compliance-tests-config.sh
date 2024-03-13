a60ea4de7924b1ed58fdfe418700a5f795effb6f
Julien Fraichot <julien.fraichot@hyland.com>
2024-03-13 16:26:33 +0100

    chore(vc-test-suite): declare work dir in test conf.ini

--- prepare-vc-compliance-tests-config.sh
+++ prepare-vc-compliance-tests-config.sh
@@ -4,8 +4,10 @@ echo "5JwtrfUV5ZZe9z3q62whK4mQduxYpa7f25UXAKJCLTN1qTiRBSF" >> pk.txt #1KeZVz9iqp
 echo "key_file=pk.txt" >> conf.ini
 mkdir data/unsigned_certificates
 mkdir data/blockchain_certificates
+mkdir data/work
 echo "unsigned_certificates_dir=${PWD}/data/unsigned_certificates" >> conf.ini
 echo "blockchain_certificates_dir=${PWD}/data/blockchain_certificates" >> conf.ini
+echo "work_dir=${PWD}/data/work" >> conf.ini
 echo "no_safe_mode" >> conf.ini
 
 echo "Prepared config file for VC compliance test suite"
