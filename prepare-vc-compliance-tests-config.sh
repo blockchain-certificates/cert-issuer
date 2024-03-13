1f4e75d415327457a560749bbde34603d5789e35
Julien Fraichot <julien.fraichot@hyland.com>
2024-03-13 16:44:19 +0100

    chore(vc-test-suite): download vc example context and allow usage by jsonld document loader

--- prepare-vc-compliance-tests-config.sh
+++ prepare-vc-compliance-tests-config.sh
@@ -1,3 +1,10 @@
+# download VC examples context
+mkdir data/context
+curl -L https://www.w3.org/2018/credentials/examples/v1 >> ${PWD}/data/context/vc-examples-v1.json
+curl -L https://www.w3.org/ns/odrl.jsonld >> ${PWD}/data/context/odrl.json
+echo "context_urls=[https://www.w3.org/2018/credentials/examples/v1, https://www.w3.org/ns/odrl.jsonld]" >> conf.ini
+echo "context_file_paths=[data/context/vc-examples-v1.json, data/context/odrl.json]" >> conf.ini
+
 echo "chain=mockchain" >> conf.ini
 echo "usb_name=${PWD}" >> conf.ini
 echo "5JwtrfUV5ZZe9z3q62whK4mQduxYpa7f25UXAKJCLTN1qTiRBSF" >> pk.txt #1KeZVz9iqpD9YeiExY2Wxu6XXexZ3pdxwe
