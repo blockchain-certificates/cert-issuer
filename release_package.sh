d32859379fe2e4471dc8c2c37080ddc809c3ba39
Julien Fraichot <julien.fraichot@hyland.com>
2024-10-16 15:41:44 +0200

    chore(CI): enable twine upload. maybe?

--- release_package.sh
+++ release_package.sh
@@ -3,6 +3,4 @@
 rm -rf dist
 python3 setup.py sdist bdist_wheel
 keyring --disable
-echo 'username '+$TWINE_USERNAME
-echo 'pwd' + $TWINE_PASSWORD
-twine upload dist/* -u%$TWINE_USERNAME% -p%$TWINE_PASSWORD% --verbose
+twine upload dist/* -u%$TWINE_USERNAME% --verbose
