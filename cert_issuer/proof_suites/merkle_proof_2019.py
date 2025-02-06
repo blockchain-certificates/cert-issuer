3a4bb280a521935912717e24fa43572a3e811331
Julien Fraichot <julien.fraichot@hyland.com>
2025-02-06 16:21:24 +0100

    feat(Issuance): allow issuer to select timezone

--- cert_issuer/proof_suites/merkle_proof_2019.py
+++ cert_issuer/proof_suites/merkle_proof_2019.py
@@ -12,19 +12,20 @@ class MerkleProof2019Suite:
     previousProof = ''
     proofValue = ''
 
-    def __init__(self, proof_value, verification_method, previous_proof = ''):
+    def __init__(self, proof_value, verification_method, issuanceTimezone='UTC'):
         self.id = 'urn:uuid:' + str(uuid.uuid4())
         self.type = DATA_INTEGRITY_PROOF_TYPE
         self.cryptosuite = MERKLE_PROOF_2019_TYPE
         self.proofPurpose = 'assertionMethod'
-        self.created = self.get_creation_time()
+        self.created = self.get_creation_time(issuanceTimezone)
         self.proofValue = proof_value.decode('utf-8')
         self.verificationMethod = verification_method
-        if previous_proof != '':
-            self.previousProof = previous_proof
 
-    def get_creation_time(self):
-        return datetime.now(timezone.utc).replace(microsecond=0, tzinfo=None).isoformat() + 'Z'
+    def get_creation_time(self, issuanceTimezone):
+        if issuanceTimezone == 'UTC':
+            return datetime.now(timezone.utc).replace(microsecond=0, tzinfo=None).isoformat() + 'Z'
+        else:
+            return datetime.now().replace(microsecond=0).isoformat() #TODO: add timezone offset
 
     def to_json_object(self):
         return self.__dict__
