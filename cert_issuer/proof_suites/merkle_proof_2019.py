ff31f8517cd481875e388043664401ac63346e33
Julien Fraichot <julien.fraichot@hyland.com>
2025-02-07 14:59:24 +0100

    refactor(MerkleProof2019): pass app config object and let MP2019 decide what to consume from it

--- cert_issuer/proof_suites/merkle_proof_2019.py
+++ cert_issuer/proof_suites/merkle_proof_2019.py
@@ -1,5 +1,5 @@
 import uuid
-from datetime import datetime, timezone
+from datetime import datetime, timezone, tzinfo
 
 DATA_INTEGRITY_PROOF_TYPE = 'DataIntegrityProof'
 MERKLE_PROOF_2019_TYPE = 'merkle-proof-2019'
@@ -12,17 +12,19 @@ class MerkleProof2019Suite:
     previousProof = ''
     proofValue = ''
 
-    def __init__(self, proof_value, verification_method, issuanceTimezone='UTC'):
+    def __init__(self, proof_value, app_config):
+        verification_method = app_config.verification_method
+        issuance_timezone = app_config.issuance_timezone or 'UTC'
         self.id = 'urn:uuid:' + str(uuid.uuid4())
         self.type = DATA_INTEGRITY_PROOF_TYPE
         self.cryptosuite = MERKLE_PROOF_2019_TYPE
         self.proofPurpose = 'assertionMethod'
-        self.created = self.get_creation_time(issuanceTimezone)
+        self.created = self.get_creation_time(issuance_timezone)
         self.proofValue = proof_value.decode('utf-8')
         self.verificationMethod = verification_method
 
-    def get_creation_time(self, issuanceTimezone):
-        if issuanceTimezone == 'UTC':
+    def get_creation_time(self, issuance_timezone):
+        if issuance_timezone == 'UTC':
             return datetime.now(timezone.utc).replace(microsecond=0, tzinfo=None).isoformat() + 'Z'
         else:
             return datetime.now().astimezone().replace(microsecond=0).isoformat()
