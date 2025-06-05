b02a60af6c78f1b778baf3134439f2865ba29752
Julien Fraichot <julien.fraichot@hyland.com>
2025-06-05 15:45:26 +0200

    feat(VC-API): partially and poorly handle errors. Maintain response object shape according to request (credentials to issue)

--- vc_api.py
+++ vc_api.py
@@ -15,30 +15,59 @@ def get_config():
         config = cert_issuer.config.get_config()
     return config
 
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    print('HANDLE ERROR', vars(e))
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response, 400
+    # pass through HTTP errors
+    # if isinstance(e, HTTPException):
+    #     return e
+
+    # now you're handling non-HTTP exceptions only
+    # return render_template("500_generic.html", e=e), 500
+
+
 @app.route('/api/v1.0/credentials/issue/', methods=['POST'])
 @app.route('/api/v1.0/credentials/issue/<handler>', methods=['POST'])
 def issue(handler=None):
     blockchain_handler = ethereum if handler == 'ethereum' else bitcoin
     config = get_config()
 
-    if not isinstance(request.json, list):
-        array_of_credentials = [request.json]
+    # maintain shape of response object as provided by the caller
+    was_array = True
+
+    if not isinstance(request.json['credential'], list):
+        was_array = False
+        array_of_credentials = [request.json['credential']]
     else:
-        array_of_credentials = request.json
+        array_of_credentials = request.json['credential']
 
     try:
         certificate_batch_handler, transaction_handler, connector = \
                 blockchain_handler.instantiate_blockchain_handlers(config, False)
         certificate_batch_handler.set_certificates_in_batch(array_of_credentials)
         cert_issuer.issue_certificates.issue(config, certificate_batch_handler, transaction_handler)
-
+        if not was_array:
+            signed_credential = certificate_batch_handler.proof[0]
+        else:
+            signed_credential = certificate_batch_handler.proof
         response = {
-            'verifiableCredential': json.dumps(certificate_batch_handler.proof)
+            'verifiableCredential': signed_credential
         }
 
         return response, 201
-    except Exception:
-        return 400
+    except Exception as e:
+        return handle_exception(e)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
